665c99a44055ce2068f804f955672d844aee8644
cman131 <cgw5994@rit.edu>
2014-01-31 17:10:48 -0500

    added level

---
+++ src/Level.rb
@@ -0,0 +1,9 @@
+class Level
+  def initialize(background, music, platforms, character, dangers)
+    @background = background
+    @music = music
+    @platforms = platforms
+    @character = character
+    @dangers = dangers
+  end
+end
