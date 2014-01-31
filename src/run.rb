98dad8af6f6d104945aec75a87cda455ea07f506
cman131 <cgw5994@rit.edu>
2014-01-31 16:55:32 -0500

    initial gui added

--- src/run.rb
+++ src/run.rb
@@ -1,4 +1,20 @@
+require 'rubygems'
+require 'rubygame'
+
 def run()
   puts "such Panda"
+  screen = Rubygame::Screen.new [640, 480], 0, 
+  [Rubygame::HWSURFACE, 
+    Rubygame::DOUBLEBUF] 
+  screen.title = "FunTime" 
+  queue = Rubygame::EventQueue.new
+  loop do
+    queue.each do |ev|
+       case ev
+       when Rubygame::QuitEvent
+         exit
+       end
+    end
+  end
 end
 run()
